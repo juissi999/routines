47e57ceaee2062f6e4312726b7b2aabc794e4ee1
juissi999 <jussi.kaatiala@gmail.com>
2020-09-29 21:29:29 +0300

    Add initial card structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,19 @@
 <script>
-  export let name
+  let routinelist = [{ description: 'eat' }, { description: 'sleep' }]
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
-<main>
-  <h1>{name}</h1>
-</main>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+</svelte:head>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>routines</h1>
+      {#each routinelist as routine}
+        <div class="card mb-1 p-2">{routine.description}</div>
+      {/each}
+    </div>
+  </div>
+</div>
