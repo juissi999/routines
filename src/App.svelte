57280e8e642aa2a5ba789d83315be0bb6a95bef5
juissi999 <jussi.kaatiala@gmail.com>
2020-09-29 21:48:32 +0300

    Add card removing with uuid ids

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
-  let routinelist = [{ description: 'eat' }, { description: 'sleep' }]
+  import { v4 as uuid } from 'uuid'
+
+  let editMode = false
+
+  let routinelist = [
+    { description: 'eat', id: uuid() },
+    { description: 'sleep', id: uuid() }
+  ]
+  const onAdd = () => {}
+  const onEdit = () => {
+    editMode = !editMode
+  }
+  const onRemove = (id) => {
+    routinelist = routinelist.filter((li) => li.id !== id)
+  }
 </script>
 
 <svelte:head>
@@ -12,8 +26,19 @@
     <div class="col">
       <h1>routines</h1>
       {#each routinelist as routine}
-        <div class="card mb-1 p-2">{routine.description}</div>
+        <div class="card mb-1">
+          <div class="card-body">
+            {#if editMode}
+              <button
+                type="button"
+                class="btn btn-info"
+                on:click={onRemove(routine.id)}>-</button>
+            {/if}
+            {routine.description}
+          </div>
+        </div>
       {/each}
+      <button on:click={onEdit}>edit</button>
     </div>
   </div>
 </div>
