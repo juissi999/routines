0292e8de77c8b2935e30280ada60ab09f109e1f3
juissi999 <jussi.kaatiala@gmail.com>
2020-10-03 16:40:58 +0300

    Create hello world service worker

---
+++ docs/sw_cached_site.js
@@ -0,0 +1,5 @@
+const cacheName = 'v1'
+
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed')
+})
--- src/App.svelte
+++ src/App.svelte
@@ -75,6 +75,15 @@
   }
 
   onMount(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () =>
+        navigator.serviceWorker
+          .register('sw_cached_site.js')
+          .then((reg) => console.log('Service Worker: Registered'))
+          .catch((err) => console.log(`Service Worker: Error: ${err}`))
+      )
+    }
+
     const initial = [
       { description: 'eat', id: uuid() },
       { description: 'sleep', id: uuid() }
